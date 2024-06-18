c6c016dc24e7bb236de8479edf84d7abb3625e6d
Pratham Malhotra <prma00001@stud.uni-saarland.de>
2024-06-18 18:28:48 +0200

    UHIiuho

--- src/ttfe/tests/SimpleTests.java
+++ src/ttfe/tests/SimpleTests.java
@@ -40,57 +40,8 @@ public class SimpleTests {
 				4 == game.getBoardHeight());
 	}
 
-///my tests
-	/*/&%$%§"§"RDHOKoNJKYBSAKÖLKWPPKOWKPÜÜ*Q?`=!==??==)(****(IIOP((/(/))))=?)?)?/&%))/Pieceadd checking piecer after and before
-	@Test
-	public void padd(){
-		
-		game.addPiece();
-		int piecepehla= game.getNumPieces();
-		game.addPiece();
-		int piecebaad= game.getNumPieces();
-		assertEquals("only piece must be added", piecepehla+1,piecebaad);
-	}
-	//if board is full
-	@Test
-	public void padd1(){
-		int pa=game.getNumPieces();
-		if (pa == 16) {
-			try {	
-				game.addPiece();
-				fail("chutiaaa");
-			} catch (IllegalStateException e) {
-			
-			}
-		}
-		else{
-			game.addPiece();
-			int piecebaad= game.getNumPieces();
-			assertEquals("lund mera", pa+1,piecebaad);
 
-		}
-	}
-	@Test
-	public void padd2(){
-		for (int row = 0; row < 4; row++) {
-            for (int col = 0; col < 3; col++) {
-                game.setPieceAt(row, col, 8);  // Set a non-2 and non-4 value, e.g., 8
-            }
-			game.addPiece();
-
-			// Check if the 4th row contains only 2 or 4
-			boolean isValidRow = true;
-			for (int col = 0; col < 4; col++) {
-				int piece = game.getPieceAt(3, col);  // Check row 3 (index 3 is the 4th row)
-				if (piece != 2 && piece != 4) {
-					isValidRow = false;
-					break;
-				}
-			}
-	
-			assertTrue("Pieces added to row 3 should have a value of 2 or 4", isValidRow);
-		}
-	}	*/
+	//test for pove possible ohne richtung
  @Test 
  public void movepoo(){
 	
@@ -179,6 +130,93 @@ public class SimpleTests {
 	assertEquals("one move is possible", true,game.isMovePossible());
  }
  }
+ //test for pove possible mit richtung richtung
+ @Test 
+ public void movepoorichtungnordenno(){
+	
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		}
+		game.setPieceAt(0, 0, 2);
+	    game.setPieceAt(0, 1, 4);
+	    game.setPieceAt(0, 2, 8);
+	    game.setPieceAt(0, 3, 16);
+
+		game.setPieceAt(1, 0, 32);
+		game.setPieceAt(1, 1, 64);
+		game.setPieceAt(1, 2, 128);
+		game.setPieceAt(1, 3, 256);
+	
+		game.setPieceAt(2, 0, 512);
+		game.setPieceAt(2, 1, 1024);
+		game.setPieceAt(2, 2, 512);
+		game.setPieceAt(2, 3, 16);
+	
+		game.setPieceAt(3, 0, 32);
+		game.setPieceAt(3, 1, 64);
+		game.setPieceAt(3, 2, 8);
+		game.setPieceAt(3, 3, 128);
+	assertEquals("no space availaible and no tiles", false,game.isMovePossible(MoveDirection.NORTH));
+ }
+ }
+ @Test 
+ public void movepoorichtungnordenyes(){
+	
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		}
+		
+		
+		game.setPieceAt(1, 0, 32);
+		game.setPieceAt(1, 1, 64);
+		game.setPieceAt(1, 2, 128);
+		game.setPieceAt(1, 3, 256);
+	
+		game.setPieceAt(2, 0, 512);
+		game.setPieceAt(2, 1, 1024);
+		game.setPieceAt(2, 2, 512);
+		game.setPieceAt(2, 3, 16);
+	
+		game.setPieceAt(3, 0, 32);
+		game.setPieceAt(3, 1, 64);
+		game.setPieceAt(3, 2, 8);
+		game.setPieceAt(3, 3, 128);
+	assertEquals("space availaiblebut not tile", true,game.isMovePossible(MoveDirection.NORTH));
+ }
+ }
+ @Test 
+ public void movepoorichtungnordenyest(){
+	
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		}
+		game.setPieceAt(0, 0, 2);
+	    game.setPieceAt(0, 1, 4);
+	    game.setPieceAt(0, 2, 8);
+	    game.setPieceAt(0, 3, 16);
+		
+		game.setPieceAt(1, 0, 32);
+		game.setPieceAt(1, 1, 64);
+		game.setPieceAt(1, 2, 128);
+		game.setPieceAt(1, 3, 16);
+	
+		game.setPieceAt(2, 0, 512);
+		game.setPieceAt(2, 1, 1024);
+		game.setPieceAt(2, 2, 512);
+		game.setPieceAt(2, 3, 16);
+	
+		game.setPieceAt(3, 0, 32);
+		game.setPieceAt(3, 1, 64);
+		game.setPieceAt(3, 2, 8);
+		game.setPieceAt(3, 3, 128);
+	assertEquals("space availaiblebut not tile", true,game.isMovePossible(MoveDirection.NORTH));
+ }
+ }
+
+
 		
 	
 
