532fbf7121948ba4e45f93b6a381067d56589ab2
Pratham Malhotra <prma00001@stud.uni-saarland.de>
2024-06-19 17:53:58 +0200

    jsjsjnnmhs

--- src/ttfe/tests/SimpleTests.java
+++ src/ttfe/tests/SimpleTests.java
@@ -311,4 +311,67 @@ public class SimpleTests {
  }
 	
 
+ @Test
+ public void emptyboardmove(){
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		} }
+		assertEquals("no move allowed", false,game.isMovePossible());
+ }
+ @Test 
+ public void movepossiblesouth(){
+	
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		} }
+		game.setPieceAt(0, 0, 2);
+		game.setPieceAt(1, 0, 4);
+		game.setPieceAt(2, 0, 8);
+		game.setPieceAt(3, 0, 16);
+	
+		game.setPieceAt(0, 1, 32);
+		game.setPieceAt(1, 1, 64);
+		game.setPieceAt(2, 1, 128);
+		game.setPieceAt(3, 1, 256);
+	
+		game.setPieceAt(0, 2, 512);
+		game.setPieceAt(1, 2, 1024);
+		game.setPieceAt(2, 2, 512);
+		game.setPieceAt(3, 2, 16);
+	
+		
+	assertEquals("no space availaible and no tiles", true,game.isMovePossible(MoveDirection.SOUTH));
+ }
+ @Test 
+ public void testpossiblewest(){
+	
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		} }
+		
+		game.setPieceAt(1, 0, 4);
+		game.setPieceAt(2, 0, 8);
+		game.setPieceAt(3, 0, 16);
+	
+		
+		game.setPieceAt(1, 1, 64);
+		game.setPieceAt(2, 1, 128);
+		game.setPieceAt(3, 1, 256);
+	
+		
+		game.setPieceAt(1, 2, 1024);
+		game.setPieceAt(2, 2, 512);
+		game.setPieceAt(3, 2, 16);
+
+		
+		game.setPieceAt(1, 3, 64);
+		game.setPieceAt(2, 3, 8);
+		game.setPieceAt(3, 3, 128);
+		
+	assertEquals("no space availaible and no tiles", true,game.isMovePossible(MoveDirection.WEST));
+ }
+ 
 }
