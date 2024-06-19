2c5ef97fc06f1fedee4eb26bcdf8c319dfa79250
Pratham Malhotra <prma00001@stud.uni-saarland.de>
2024-06-19 05:29:00 +0200

    UHIiuhgosdj,kd

--- src/ttfe/tests/SimpleTests.java
+++ src/ttfe/tests/SimpleTests.java
@@ -171,28 +171,55 @@ public class SimpleTests {
 		} }
 		
 	
-	game.setPieceAt(1, 0, 4);
-	game.setPieceAt(2, 0, 8);
-	game.setPieceAt(3, 0, 16);
 
-
+	game.setPieceAt(0, 1, 32);
 	game.setPieceAt(1, 1, 64);
 	game.setPieceAt(2, 1, 128);
 	game.setPieceAt(3, 1, 256);
 
-	
+	game.setPieceAt(0, 2, 512);
 	game.setPieceAt(1, 2, 1024);
 	game.setPieceAt(2, 2, 512);
 	game.setPieceAt(3, 2, 16);
 
-	
+	game.setPieceAt(0, 3, 32);
 	game.setPieceAt(1, 3, 64);
 	game.setPieceAt(2, 3, 8);
 	game.setPieceAt(3, 3, 128);
 	assertEquals("space availaible but not tile", true,game.isMovePossible(MoveDirection.NORTH));
 
  }
+ /* 
+ @Test 
+ public void movepoorichtungnordenyest(){
+	
+	for (int row = 0; row < 4; row++) {
+		for (int col = 0; col < 4; col++) {
+			game.setPieceAt(row, col, 0);  
+		} }
+	game.setPieceAt(0, 0, 2);
+	game.setPieceAt(1, 0, 4);
+	game.setPieceAt(2, 0, 8);
+	game.setPieceAt(3, 0, 16);
 
+	game.setPieceAt(0, 1, 32);
+	game.setPieceAt(1, 1, 64);
+	game.setPieceAt(2, 1, 128);
+	game.setPieceAt(3, 1, 256);
+
+	game.setPieceAt(0, 2, 512);
+	game.setPieceAt(1, 2, 1024);
+	game.setPieceAt(2, 2, 512);
+	game.setPieceAt(3, 2, 512);
+
+	game.setPieceAt(0, 3, 32);
+	game.setPieceAt(1, 3, 64);
+	game.setPieceAt(2, 3, 8);
+	game.setPieceAt(3, 3, 128);
+
+	assertEquals("no space availaible but  tiles are", true,game.isMovePossible(MoveDirection.NORTH));
+
+ }*/
 
 
 		
